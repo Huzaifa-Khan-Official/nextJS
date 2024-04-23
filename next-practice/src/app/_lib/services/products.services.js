760238e4a845ea8e61bdefff2168b20e919f0036
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2024-04-23 17:59:44 +0500

    fetch data done

--- next-practice/src/app/_lib/services/products.services.js
+++ next-practice/src/app/_lib/services/products.services.js
@@ -1,6 +1,6 @@
 export const getProducts = async () => {
     try {
-        const fetchData = await fetch('https://dummyjson.com/products');
+        const fetchData = await fetch(`${process.env.NEXT_SERVER_URL}/products`);
         const data = await fetchData.json();
         return data;
     } catch (error) {
@@ -10,7 +10,7 @@ export const getProducts = async () => {
 
 export const getProductDetail = async (id) => {
     try {
-        const fetchData = await fetch(`https://dummyjson.com/products/${id}`);
+        const fetchData = await fetch(`${process.env.NEXT_SERVER_URL}/products/${id}`);
         const data = await fetchData.json();
         return data
     } catch (error) {
--- next-practice/src/app/products/[pid]/page.jsx
+++ next-practice/src/app/products/[pid]/page.jsx
@@ -1,13 +1,22 @@
 import { getProductDetail } from "@/app/_lib/services/products.services";
+import Link from "next/link";
+
+export async function generateMetadata({ params }) {
+  const { pid } = params;
+  const productDetail = await getProductDetail(pid);
+  if (!productDetail || productDetail.message) return { title: "Product Not Found" }
+  return {
+    title: productDetail.title
+  }
+}
 
 export default async function ProductDetailPage({ params }) {
-  const { pid } = params
+  const { pid } = params;
   const productDetail = await getProductDetail(pid);
-  if (!productDetail) return <h3>Product Not Found!</h3>
-
-  console.log(productDetail);
+  if (!productDetail || productDetail.message) return <h3>Product Not Found!</h3>
   return (
     <div>
+      <Link href="/products"><p>Back To Home:</p></Link>
       {
         <div>
           <h1>
